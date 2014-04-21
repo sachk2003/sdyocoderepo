9a265627dec6627a683974b9bb097bef0e0a7ac4
pathardepavan <pathardepavan@gmail.com>
2014-04-21 20:59:45 +0530

    discountbylistfooter
    discountbylistfooter  changes

--- dev/application/views/templates/discountbylistfooter.php
+++ dev/application/views/templates/discountbylistfooter.php
@@ -1,12 +1,12 @@
 <footer id="footer-1" class="footer">
                 <div class="container">
                     <div class="row">
-                    	<div class="col-md-2"><a href="vendor/index.php" >Vendor Login</a></div>
-                        <div class="col-md-2"><a href="about.php" >About Us</a></div>
-                        <div class="col-md-2 "><a href="privacy.php">Privacy</a></div>
-                        <div class="col-md-2 "><a href="help.php">Help</a></div>
-                        <div class="col-md-2 "><a href="jobs.php">Jobs</a></div>
-                        <div class="col-md-2 "><a href="contact.php">Contact</a></div>
+                    	<div class="col-md-2"><a href="../vendor/index.php" >Vendor Login</a></div>
+                        <div class="col-md-2"><a href="../about.php" >About Us</a></div>
+                        <div class="col-md-2 "><a href="../privacy.php">Privacy</a></div>
+                        <div class="col-md-2 "><a href="../help.php">Help</a></div>
+                        <div class="col-md-2 "><a href="../jobs.php">Jobs</a></div>
+                        <div class="col-md-2 "><a href="../contact.php">Contact</a></div>
                                               
                         
                     </div>
