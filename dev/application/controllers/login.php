e04ebbfbfee1214ebde2bafbb7e7639193ca7a8e
Pavan <pathardepavan@gmail.com>
2014-04-27 00:01:46 +0530

    Update login.php

--- dev/application/controllers/login.php
+++ dev/application/controllers/login.php
@@ -15,7 +15,7 @@ class Login extends CI_Controller {
 	 function __construct(){
 
         parent::__construct();
-		$this->load->helper(array('form'));
+		$this->load->helper(array('form','url'));
 		$this->load->model('vendorfunctions');
                $this->force_ssl();
 		           
