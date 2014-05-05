95a5501e8b52973d80812c7f3b1d3ca86e6327af
Pavan <pathardepavan@gmail.com>
2014-05-05 22:05:13 +0530

    Update homeheader.php

--- dev/application/views/templates/homeheader.php
+++ dev/application/views/templates/homeheader.php
@@ -44,7 +44,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             </button>
-                            <a class="navbar-brand" href="http://<?php echo base_url('');?>">Super<span>Dealyo</span></a>
+                            <a class="navbar-brand" href="<?php echo base_url('');?>">Super<span>Dealyo</span></a>
                         </div>
                         <!-- Collect the nav links, forms, and other content for toggling -->
                         <div class="collapse navbar-collapse navbar-ex1-collapse">
