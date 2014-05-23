0f2e8fca3a5acba90e91f0598d447635b9be4c88
pathardepavan <pathardepavan@gmail.com>
2014-05-23 19:38:59 +0530

    User details

---
+++ dev/application/views/templates/loggedinusermenu.php
@@ -0,0 +1,13 @@
+<div class="container">
+	<div class="row">
+  			<div class="col-md-3" id="leftCol">
+              
+              	<ul class="nav nav-stacked" id="sidebar">
+                  <li><a href="../user/index">Welcome</a></li>
+                  <li><a href="update_profile">Update Profile</a></li>
+                  
+                  <li><a href="profile_delete">Delete Profile</a></li>
+                  <li><a href="logout">Logout</a></li>	
+                  
+              	</ul>
+           </div> 
