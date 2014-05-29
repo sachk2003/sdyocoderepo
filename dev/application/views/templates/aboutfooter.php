ee7b2c9e02cbe7d962f908e6278568f54aa33f88
Pavan <pathardepavan@gmail.com>
2014-05-30 00:02:45 +0530

    Update aboutfooter.php

--- dev/application/views/templates/aboutfooter.php
+++ dev/application/views/templates/aboutfooter.php
@@ -1,5 +1,5 @@
 <footer id="footer-1" class="footer">
-                <div class="container">
+                <div class="container" style="width:100%">
                     <div class="row">
                         <div class="col-md-2"><a href="login/index" >Vendor Login</a></div>
                         <div class="col-md-2"><a href="userlogin/index" >User Login</a></div>
