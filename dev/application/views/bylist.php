9af9acd9536eece03cc44e73b1f77a742915243e
pathardepavan <pathardepavan@gmail.com>
2014-06-03 19:58:54 +0530

    bylist.php update

--- dev/application/views/bylist.php
+++ dev/application/views/bylist.php
@@ -61,7 +61,7 @@
 <div class="row">
    <div class="col-md-4"></div>
    <div class="col-md-4">
-  <h5>Sign Up for email alerts for your shopping list  <a href="../userlogin/"><button type="button" class="btn btn-primary">User Login</button></a> </h5></div>
+  <h5>Sign Up for email alerts for your shopping list <a href="../userlogin/"><button type="button" class="btn btn-primary">User Login</button></a> </h5></div>
    <div class="col-md-4"></div>
 </div> 
 
