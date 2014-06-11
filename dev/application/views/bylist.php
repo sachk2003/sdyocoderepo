ca540fd1e77870d29a9a366f09540e83bb421dcb
pathardepavan <pathardepavan@gmail.com>
2014-06-11 23:05:35 +0530

    bylist

--- dev/application/views/bylist.php
+++ dev/application/views/bylist.php
@@ -54,15 +54,16 @@
             <div class="col-md-3"></div>
             
             
-              </div>            	
-            </div>
-            
-
-<div class="row">
+              </div>  
+              <div class="row">
    <div class="col-md-4"></div>
    <div class="col-md-4">
   <h5>Sign Up for email alerts for your shopping list <a href="../userlogin/"><button type="button" class="btn btn-primary btn-xs">User Login</button></a> </h5></div>
    <div class="col-md-4"></div>
-</div> 
+</div>           	
+            </div>
+            
+
+
 
 <?php $this->load->view('templates/bylistfooter');?>
