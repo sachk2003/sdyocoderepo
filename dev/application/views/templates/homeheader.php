28954460a481f9b5a2d269337145970e91358567
pathardepavan <pathardepavan@gmail.com>
2014-06-12 20:14:19 +0530

    newhome

--- dev/application/views/templates/homeheader.php
+++ dev/application/views/templates/homeheader.php
@@ -45,7 +45,7 @@
                             <span class="icon-bar"></span>
                             </button>
                             <!--<a class="navbar-brand" href="<?php echo base_url('');?>">Super<span>Dealyo</span></a>-->
-                            <a class="navbar-brand" href="<?php echo base_url('');?>"><img src="<?php echo base_url('assets/img/logo.png')?>" height="50px"/></a>
+                            <a class="navbar-brand" href="<?php echo base_url('');?>"><img src="<?php echo base_url('assets/img/logo.png')?>" height="45px"/></a>
                         </div>
                         <!-- Collect the nav links, forms, and other content for toggling -->
                         <div class="collapse navbar-collapse navbar-ex1-collapse">
