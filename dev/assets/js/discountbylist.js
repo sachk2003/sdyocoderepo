1e6baa91d4f2d9e200b1a02f3fb98296593d25d4
pathardepavan <pathardepavan@gmail.com>
2014-06-17 23:34:59 +0530

    latest changes

--- dev/assets/js/discountbylist.js
+++ dev/assets/js/discountbylist.js
@@ -86,13 +86,10 @@ tpj('document').ready(function(){
 			                                if(element.bsin!=null)
 			                             	message+='<li>Brand Code: '+element.bsin+'</li>';
 			                             	
-			                             	if(element.brandnm!=null || element.brandnm!=" ")
+			                             	if(element.brandnm!='')
 			                             	message+='<li>Brand Name: '+element.brandnm+'</li>';
-			                             	if(element.brandnm==null) console.log('true');
-			                             	if(element.brandnm=='') console.log('spacetrue');
-			                             	if(element.brandnm==' ') console.log('space2true');
-			                             	  console.log('Brand Name:'+element.brandnm);
-			                             	if(element.brandtypename!=null || element.brandtypename!=" ")
+			                             	
+			                             	if(element.brandtypename!='')
 			                             	message+='<li>Brand Type: '+element.brandtypename+'</li>';
 			                             	console.log(message);
 			                             	
