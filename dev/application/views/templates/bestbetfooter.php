cbc98cd5535cdeaff98569e8e1582611e9b6d595
pathardepavan <pathardepavan@gmail.com>
2014-07-08 22:19:52 +0530

    new changes

--- dev/application/views/templates/bestbetfooter.php
+++ dev/application/views/templates/bestbetfooter.php
@@ -124,25 +124,12 @@
 			  content: function()
 			            {   //console.log("entered the popoer content block");
 			            
-			               tpj(this).data('bs.tooltip','false').tooltip(
+			               tpj(this).data('bs.tooltip','false').popover(
 			               	{
-			               	  title: 'Items',
-			               	  placement:function (context, source) {
-						        var position = tpj(source).position();
-						        var width = window.innerWidth;
-						        var positionleft = tpj(source).offset().left;
-						        //alert(width - positionleft);
-						       if((width - positionleft)<250 && width >500)
-						          return "left";         
-						        else if(width < 500)
-						           return "auto";
-						        else if(width < 800)
-						           return "auto";    
-						        else   
-						          return "right"; 
-			               	},
-			               	content:function(){
-			               	     var message='';
+			               	  title: 'Items'
+			                } 	  
+			               
+			               	var message='';
 			            	var vname =tpj(this).attr("name");
 			            	if(vname!='')
 			            	{
@@ -164,19 +151,9 @@
 			               		
 			               	}
 			               	
-			               	}
-			               );
-			                
-			            	
-			            
-			    }
-			    
-			    });
-
-              
-                
-                
-            
+			               	
+			              
+			               
             });   
                 
                 
