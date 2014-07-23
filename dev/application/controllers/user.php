97e1c69abdd021c96f4607d3106a6cdddcfbdc72
pathardepavan <pathardepavan@gmail.com>
2014-07-23 22:11:45 +0530

    User Changes

--- dev/application/controllers/user.php
+++ dev/application/controllers/user.php
@@ -105,11 +105,17 @@ class User extends CI_Controller {
 		$mphone = $this->input->post('mphone');
 		$email = $this->input->post('email2');
 		
-		$alertitem1 = $this->input->post('alertitem1');
-		$alertitem2 = $this->input->post('alertitem2');
-		$alertitem3 = $this->input->post('alertitem3');
-		$alertitem4 = $this->input->post('alertitem4');
-		$alertitem5 = $this->input->post('alertitem5');
+		$aitem1 = $this->input->post('alertitem1');
+		$aitem2 = $this->input->post('alertitem2');
+		$aitem3 = $this->input->post('alertitem3');
+		$aitem4 = $this->input->post('alertitem4');
+		$aitem5 = $this->input->post('alertitem5');
+		
+		$ib1=explode('upc:',$aitem1); $alertitem1=$ib1[1];
+        $ib2=explode('upc:',$aitem2); $alertitem2=$ib2[1]; 		
+		$ib3=explode('upc:',$aitem3); $alertitem3=$ib3[1];
+		$ib4=explode('upc:',$aitem4); $alertitem4=$ib4[1];
+		$ib5=explode('upc:',$aitem5); $alertitem5=$ib5[1];
 		
 		$password = "temp".$fname;
 		
@@ -143,6 +149,55 @@ class User extends CI_Controller {
 		 }/* End of Zip Code else*/
 		
 	}
+   
+   
+   
+   	function change_password()
+    {
+				$data['message']='';
+			    $this->load->view('user/changepassword',$data);
+				
+							
+	}
+	
+    function change_password_submit()
+			{
+				$email=$this->input->post('email2');
+				$password=$this->input->post('password2');
+				$newpassword=$this->input->post('newpassword');
+				$message='';
+				
+				
+				$validity=$this->userfunctions->validate($email,$password);
+				//var_dump($validity);
+				
+				if($validity)
+				{
+					
+					if($this->vendorfunctions->updateuser($email,$newpassword))
+					{
+						$data['message']='Password Updated';
+			            $this->load->view('user/changepassword',$data);
+						
+					}
+					else{
+						$data['message']='Problem updating password';
+			            $this->load->view('user/changepassword',$data);
+						
+					}
+					
+					
+					
+				}
+				else{
+					$data['message']='Username/Password is Wrong';
+			        $this->load->view('user/changepassword',$data);
+					
+				}
+				
+				
+			}
+
 
     public function profile_delete()
 	{
